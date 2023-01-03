4031d1a8591b6c3780021f0e3bb044160740ad68
example <61242448+berkaycobanbas@users.noreply.github.com>
2023-01-03 23:45:56 +0300

    UpdateTwo

--- home.dart
+++ home.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main(List<String> args) {
   //String veri tipi çalışmaları
   String ad = "Berkay";
@@ -77,4 +79,98 @@ void main(List<String> args) {
     default:
       print("Bu sayi tektir.");
   }
+
+/////////////////////////////////////////////////////////////////
+
+// ARITMETIK OPERATORLER
+
+  double s1 = 12;
+  double s2 = 11;
+
+  print("$s1 + $s2 sayilarinin toplami: ${s1 + s2}");
+  print("$s1 - $s2 sayilarinin farki: ${s1 - s2}");
+  print("$s1 * $s2 sayilarinin çarpimi: ${s1 * s2}");
+  print("$s1 / $s2 sayilarinin bolumu: ${s1 / s2}");
+  print("$s1 % $s2 sayilarinin modu: ${s1 % s2}");
+
+/////////////////////////////////////////////////////////////////
+
+// ATAMA VE KARSILASTIRMA
+
+  double s3 = 8;
+  s3 = s3 + 8;
+  print(s3);
+
+  s3 += 8;
+  print(s3);
+
+  s3 %= 2;
+  print(s3);
+
+  // < : Kucuktur.
+  // > : buyuktur.
+  // <= : kucuk veya esit.
+  // >= : buyuk veya esit.
+  // == : esit mi?.
+  // != = esit değil.
+// bu karsilastirmalar string ifadelerde kullanılabilir.
+  double s4 = 2;
+  double s5 = 1;
+  if (s4 != s5) {
+  } else {
+    print("$s4 ve $s5 birbirine esittir.");
+  }
+
+/////////////////////////////////////////////////////////////////
+
+  // MANTIKSAL OPERTAORLER
+
+  //&& , ||
+  // && = ve
+  // || = veya
+  // ! = değil
+
+  int s6 = 5;
+  int s7 = 6;
+
+  if (s6 > 0 && s7 > 0) {
+    print("$s6 ve $s7 pozitif sayilardir");
+  } else if (s6 < 0 || s7 < 0) {
+    print("$s6 ve $s7 sayisinin carpimi negatiftir.");
+  }
+
+/////////////////////////////////////////////////////////////////
+
+  // ARTTIRMA AZALTMA OPERATORLERI
+
+  // ++ -> degiskeni 1 arttirir
+  // -- -> degiskeni 1 azaltir
+  // bu ifadelerin degiskenin saginda veya solunda olmasi onemlidir.
+// sayi++ -> önce sayiyi kullan sonra 1 arttır, ++ sayi ise önce sayiyi 1 arttır sonra kullan anlamina gelir.
+
+  int s8 = 11;
+  s8 = s8 + 1;
+  s8 += 8;
+  print(s8);
+
+  s8++;
+  print(s8);
+
+  int s9 = 10;
+
+  print(s9++); // önce yazdır, sonra arttır.
+  print(++s9); // önce arttır, sonra yazdır.
+
+// Bu kosullar azaltma icinde gecerlidir.
+
+/////////////////////////////////////////////////////////////////
+
+// ISLEM ONCELIGI
+
+// () -> once parantez ici isletilir.
+// ++ ve -- degiskenden once gelenler
+// * ve /
+// + ve -
+// = atama islemleri
+// ++ ve --degiskenden sonra gelenler
 }
