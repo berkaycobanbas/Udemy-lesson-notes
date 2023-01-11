44ed7704cb70ec7ed72c819cedc805c9e34aafa8
example <61242448+berkaycobanbas@users.noreply.github.com>
2023-01-11 23:38:50 +0300

    Introduction to functions

---
+++ homework_three.dart
@@ -0,0 +1,10 @@
+// Function concept
+void main(List<String> args) {
+  calculatePerimeter();
+}
+
+calculatePerimeter() {
+  int weight = 10, height = 10;
+  int perimeter = (weight * 2) + (height * 2);
+  print("Perimeter = $perimeter");
+}
